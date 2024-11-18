b53c8be932b678ffc10d2d3f1a326b1e962ad413
Duke-2406 <deepaksihare891@gmail.com>
2024-11-18 09:59:55 +0530

    State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // import logo from './logo.svg';
-import Greet from './components/Greet';
+// import Greet from './components/Greet';
 import Welcome from './components/Welcome';
+// import Message from './components/Message';
+// import Counter from './components/Counter';
 import './App.css';
 import React, { Component } from 'react';
 
@@ -8,16 +10,18 @@ class App extends Component {
   render(){
     return (
       <div className='App'>
-        <Greet name="Bruce" heroName="Batman">
+        {/* <Counter /> */}
+        {/* <Message /> */}
+        {/* <Greet name="Bruce" heroName="Batman">
           <p>This is children props</p>
         </Greet>
         <Greet name="Clark" heroName="Superman">
           <button>Action</button>  
-        </Greet>
-        <Greet name="Diana" heroName="Wonder Woman"/>
+        </Greet> */}
+        {/* <Greet name="Diana" heroName="Wonder Woman"/> */}
         <Welcome name="Bruce" heroName="Batman" />
-        <Welcome name="Clark" heroName="Superman" />
-        <Welcome name="Diana" heroName="Wonder Woman" />
+        {/* <Welcome name="Clark" heroName="Superman" />
+        <Welcome name="Diana" heroName="Wonder Woman" /> */}
       </div>
     )
   }
